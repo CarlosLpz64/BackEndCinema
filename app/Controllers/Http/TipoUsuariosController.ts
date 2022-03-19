290a5c6170c35965fb7d7c68c2bb5366567e82d6
Mariano Rodriguez <101942800+MarianoRodriguezx@users.noreply.github.com>
2022-03-19 14:14:16 -0600

    commit de controller

---
+++ app/Controllers/Http/TipoUsuariosController.ts
@@ -0,0 +1,24 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import TUsuario from 'App/Models/TUsuario'
+import User from 'App/Models/User'
+import authConfig from 'Config/auth'
+
+export default class TipoUsuariosController {
+    //public async VerificarExistencia({response, params, auth}: HttpContextContract){
+
+        //try{
+        //    await User.findOrFail(params.id)
+
+        //    try{
+        //        await 
+        //    }
+        //    catch(error){
+                
+        //    }
+
+        //}
+        //catch(error){
+
+        //}
+    //}
+}
